e8cd4964d3e0614396d562bf28e76d3713a51404
Nadzeya Pekarskaya <napekarskaya@gmail.com>
2024-01-10 09:26:48 +0100

    changed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
-// const URL = 'mongodb+srv://napekarskaya:nadia060290i@cluster0.e3cmski.mongodb.net/postsbase';
+const URL = 'mongodb+srv://napekarskaya:nadia060290i@cluster0.e3cmski.mongodb.net/postsbase';
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -31,7 +31,7 @@ app.use(postRoutes);
 
 // Подключение к MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
     console.error('DB Connection error:', err);
